ba7bc0ba326b374eaef6dbe3f1ea23cce7cdcfa9
Truilin <971960483@qq.com>
2015-10-21 08:16:28 -0700

    Java connect redis

---
+++ src/main/java/com/trl/redistest/Main.java
@@ -0,0 +1,14 @@
+package com.trl.redistest;
+
+/**
+ * Created by truilin on 10/20/15.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        new RedisClient().show();
+    }
+
+}
